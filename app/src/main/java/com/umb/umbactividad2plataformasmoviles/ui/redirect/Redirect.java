ed153a029b1e8a790649f16949c36931d4873364
ncastellanos <nelsongarzon0000@gmail.com>
2021-10-02 22:17:26 -0500

    feature - Agregar vista de redirección a la página de la universidad

--- app/src/main/java/com/umb/umbactividad2plataformasmoviles/ui/redirect/Redirect.java
+++ app/src/main/java/com/umb/umbactividad2plataformasmoviles/ui/redirect/Redirect.java
@@ -11,18 +11,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.umb.umbactividad2plataformasmoviles.R;
 import com.umb.umbactividad2plataformasmoviles.databinding.FragmentHomeBinding;
 
 public class Redirect extends Fragment {
 
     private FragmentHomeBinding binding;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-        return root;
+        View view = inflater.inflate(R.layout.fragment_redirect, container, false);
+        Button button = view.findViewById(R.id.buttonGoUMB);
+          button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                goUmb();
+            }
+        });
+        return view;
     }
 
     @Override
@@ -31,8 +41,8 @@ public class Redirect extends Fragment {
         binding = null;
     }
 
-    public void goUmb(View view) {
-         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://umb.edu.co/"));
-                        startActivity(browserIntent);
+    public void goUmb() {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://umb.edu.co/"));
+        startActivity(browserIntent);
     }
 }
